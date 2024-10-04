8919a6884520bada47f4992eea0358ee8126b0a6
TuanDinh140194 <tnd@pdx.edu>
2024-10-04 00:09:48 -0700

    npm build 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from './sections/NavBar'
+import NavBar from './sections/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
